eaee08caeab6ad5fa07c6f9f7c12f0b6815eb398
Khushi Rai <KhushpreetKaurRai@student.ufv.ca>
2021-05-17 15:07:21 -0400

    added photo gallery

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -19,8 +19,7 @@ import { ErrorInterceptor } from './_interceptors/error.interceptor';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
 import { MemberCardComponent } from './members/member-card/member-card.component';
-
-
+import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 
 @NgModule({
   declarations: [
@@ -45,9 +44,10 @@ import { MemberCardComponent } from './members/member-card/member-card.component
     AppRoutingModule,
     SharedModule
   ],
-  // multi true make sures we are adding interceptors, but not replacing the original ones
+  // multi true make sure we are adding interceptors, but not replacing the original ones
   providers: [
-    {provide:HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi:true}
+    {provide:HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi:true},
+    {provide:HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi:true}
   ],
   bootstrap: [AppComponent]
 })
