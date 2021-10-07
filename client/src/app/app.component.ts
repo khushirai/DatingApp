0783111f0ee6d9e1b0db47909658887123065a51
Khushi Rai <KhushpreetKaurRai@student.ufv.ca>
2021-10-07 09:44:19 -0400

    added signalR

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { User } from './_models/user';
 import { AccountService } from './_services/account.service';
+import { PresenceService } from './_services/presence.service';
 
 
 @Component({
@@ -13,7 +14,7 @@ export class AppComponent implements OnInit {
   title = 'The Dating App';
   users: any;
 
-  constructor(private accountService: AccountService ) {}
+  constructor(private accountService: AccountService, private presence: PresenceService ) {}
 
   // this req will be completed because it is an http req
   ngOnInit() {
@@ -22,6 +23,10 @@ export class AppComponent implements OnInit {
 
   setCurrentUser( ){
     const user: User= JSON.parse(localStorage.getItem('user'));
-    this.accountService.setCurrentUser(user);
+    if(user){
+      this.accountService.setCurrentUser(user);
+      this.presence.createHubConnection(user);
+    }
+  
   }
 }
